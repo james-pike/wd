049e6eb760d9c96db18d3d8f5149f249750bc9c1
James <jamesandrewpike@gmail.com>
2024-12-02 13:19:03 -0800

    commit

--- src/components/widgets/Header.tsx
+++ src/components/widgets/Header.tsx
@@ -1,98 +1,223 @@
-import { component$, useStore } from "@builder.io/qwik";
-import { useContent } from "@builder.io/qwik-city";
+import {
+  $,
+  PropsOf,
+  component$,
+  useSignal,
+  useStyles$,
+} from '@builder.io/qwik';
 
-import Logo from "~/components/common/Logo";
-import ToggleTheme from "~/components/common/ToggleTheme";
-import ToggleMenu from "~/components/common/ToggleMenu";
-import IconChevronDown from "../icons/IconChevronDown";
+import { useLocation } from '@builder.io/qwik-city';
+import { Modal } from '@qwik-ui/headless';
+import { useAppState } from '~/_state/use-app-state';
+
+import { cn } from '@qwik-ui/utils';
+
+
+
+
+import { useTheme } from '~/lib/provider';
+import Theme from '../theme';
+import { Button, buttonVariants } from '../button';
+
+
+export interface HeaderProps {
+  showVersion?: boolean;
+  showBottomBorder?: boolean;
+}
 
 export default component$(() => {
-  const store = useStore({
-    isScrolling: false,
-  });
+  useStyles$(`
+    .sidebar-mobile::backdrop {
+      background: rgba(0,0,0,0.5);
+    }
+  
+    .sidebar-mobile {
+      animation: sidebarOpen 0.75s forwards cubic-bezier(0.6, 0.6, 0, 1);
+    }
+  
+    .sidebar-mobile::backdrop {
+      animation: sidebarFadeIn 0.75s forwards cubic-bezier(0.6, 0.6, 0, 1);
+    }
+  
+    .sidebar-mobile.modal-closing {
+      animation: sidebarClose 0.35s forwards cubic-bezier(0.6, 0.6, 0, 1);
+    }
+  
+    .sidebar-mobile.modal-closing::backdrop {
+      animation: sidebarFadeOut 0.35s forwards cubic-bezier(0.6, 0.6, 0, 1);
+    }
 
-  const { menu } = useContent();
+    @keyframes sidebarOpen {
+      from {
+        opacity: 0;
+        transform: translateX(100%);
+      }
+      to {
+        opacity: 1;
+        transform: translateX(0%);
+      }
+    }
+  
+    @keyframes sidebarClose {
+      from {
+        opacity: 1;
+        transform: translateX(0%);
+      }
+      to {
+        opacity: 0;
+        transform: translateX(100%);
+      }
+    }
+  
+    @keyframes sidebarFadeIn {
+      from {
+        opacity: 0;
+      }
+      to {
+        opacity: 1;
+      }
+    }
+  
+    @keyframes sidebarFadeOut {
+      from {
+        opacity: 1;
+      }
+      to {
+        opacity: 0;
+      }
+    }
+    `);
+
+  
+
+  const rootStore = useAppState();
+  const isSidebarOpenedSig = useSignal(false);
+  const location = useLocation();
+
+  const isDocsActive = (baseHref: string) => {
+    const isLinkActive = location.url.pathname.startsWith(baseHref);
+    return `
+        transition-color ease-step duration-300 ${isLinkActive ? 'font-bold' : ''}`;
+  };
+
+
+
+  const { themeSig } = useTheme();
 
   return (
-    <header
-      id="header"
-      class={`sticky top-0 z-40 flex-none mx-auto w-full border-b border-gray-50/0 transition-[opacity] ease-in-out ${
-        store.isScrolling
-          ? " md:bg-white/90 md:backdrop-blur-sm dark:md:bg-slate-900/90 bg-white dark:bg-slate-900"
-          : ""
-      }`}
-      window:onScroll$={() => {
-        if (!store.isScrolling && window.scrollY >= 10) {
-          store.isScrolling = true;
-        } else if (store.isScrolling && window.scrollY < 10) {
-          store.isScrolling = false;
-        }
-      }}
+    <Modal.Root
+      class={cn(
+        'sticky top-0 z-10 flex h-16 justify-center border-b bg-background',
+        themeSig.value?.includes('brutalist') && 'border-b-2',
+      )}
+      bind:show={isSidebarOpenedSig}
     >
-      <div class="absolute inset-0"></div>
-      <div class="relative text-default py-3 px-3 md:px-6 mx-auto w-full md:flex md:justify-between max-w-7xl">
-        <div class="mr-auto rtl:mr-0 rtl:ml-auto flex justify-between">
-          <a class="flex items-center" href={"/"}>
-            <Logo />
-            
+      <header class="flex w-full max-w-screen-2xl items-center justify-between">
+        <section class="flex items-center justify-start">
+          <a href="/" aria-label="Qwik UI Logo" class="ml-4">
+            {/* <LogoWithBorders class="hidden sm:block" />
+            <LogoIcon class="block sm:hidden" /> */}
           </a>
-          <div class="flex items-center md:hidden">
-            <ToggleTheme iconClass="w-6 h-6 md:w-5 md:h-5 md:inline-block" />
-            <ToggleMenu />
-          </div>
-        </div>
-        <nav
-          class="items-center w-full md:w-auto hidden md:flex text-default overflow-y-auto overflow-x-hidden md:overflow-y-visible md:overflow-x-auto md:mx-5"
-          aria-label="Main navigation"
-        >
-          {menu && menu.items ? (
-            <ul class="flex flex-col md:flex-row md:self-center w-full md:w-auto text-xl md:text-[0.9375rem] tracking-[0.01rem] font-medium">
-              {menu.items.map(({ text, href, items }, key) => (
-                <li key={key} class={items?.length ? "dropdown" : ""}>
-                  {items?.length ? (
-                    <>
-                      <button class="hover:text-link dark:hover:text-white px-4 py-3 flex items-center">
-                        {text} <IconChevronDown class="w-3.5 h-3.5 ml-0.5 rtl:ml-0 rtl:mr-0.5 hidden md:inline" />
-                      </button>
-                      <ul class="dropdown-menu md:backdrop-blur-md dark:md:bg-slate-800 rounded md:absolute pl-4 md:pl-0 md:hidden font-medium md:bg-white/90 md:min-w-[200px] drop-shadow-xl">
-                        {items.map(({ text: text2, href: href2 }, key2) => (
-                          <li key={key2}>
-                            <a
-                              class="first:rounded-t last:rounded-b md:hover:bg-gray-100 hover:text-link dark:hover:text-white dark:hover:bg-gray-700 py-2 px-5 block whitespace-no-wrap"
-                              href={href2}
-                            >
-                              {text2}
-                            </a>
-                          </li>
-                        ))}
-                      </ul>
-                    </>
-                  ) : (
-                    <a class="hover:text-link dark:hover:text-white px-4 py-3 flex items-centers" href={href}>
-                      {text}
-                    </a>
-                  )}
-                </li>
-              ))}
-            </ul>
-          ) : null}
-        </nav>
-        <div class="hidden md:self-center md:flex items-center md:mb-0 fixed w-full md:w-auto md:static justify-end left-0 rtl:left-auto rtl:right-0 bottom-0 p-3 md:p-0">
-          <div class="items-center flex justify-between w-full md:w-auto">
-            <div class="flex">
-              <ToggleTheme iconClass="w-6 h-6 md:w-5 md:h-5 md:inline-block" />
-            </div>
-            <span class="ml-4 rtl:ml-0 rtl:mr-4">
-              <a
-                href="https://github.com/onwidget/qwind"
-                class="btn btn-primary ml-2 py-2.5 px-5.5 md:px-6 font-semibold shadow-none text-sm w-auto"
-              >
-                Download
+     
+        </section>
+
+        <div class="mr-4 flex items-center">
+          <div class="mr-6 hidden items-center space-x-8 text-sm lg:flex">
+            <a
+              class={isDocsActive('/docs/headless/')}
+              href="/docs/headless/introduction/"
+            >
+              Headless
+            </a>
+            {rootStore.featureFlags?.showStyled && (
+              <a class={isDocsActive('/docs/styled/')} href="/docs/styled/introduction/">
+                Styled
               </a>
-            </span>
+            )}
+          </div>
+          <div class="flex items-center space-x-1 xs:space-x-4">
+            <Theme />
+            <a
+              href="https://discord.gg/PVWUUejrez"
+              target="_blank"
+              class={cn(buttonVariants({ size: 'icon', look: 'ghost' }))}
+            >
+              {/* <DiscordIcon /> */}
+            </a>
+            <a
+              target="_blank"
+              href="https://github.com/qwikifiers/qwik-ui"
+              aria-label="Qwik-UI GitHub repository"
+              class={cn(buttonVariants({ size: 'icon', look: 'ghost' }))}
+            >
+              {/* <GitHubIcon /> */}
+            </a>
+            <DarkModeToggle />
+            <Button
+              type="button"
+              aria-label="Toggle navigation"
+              onClick$={() => {
+                isSidebarOpenedSig.value = !isSidebarOpenedSig.value;
+              }}
+              size="icon"
+              look="ghost"
+              class="flex lg:hidden"
+            >
+              {/* <LuMenu class="h-6 w-6" /> */}
+            </Button>
           </div>
         </div>
+      </header>
+      <Modal.Panel class="sidebar-mobile ml-auto mr-0 h-screen w-full min-w-80 max-w-sm rounded-base border-0 bg-background p-8 text-foreground shadow-md">
+        <div class="mb-2 pb-4 pt-2">
+          {/* <DocsNavigation
+            linksGroups={
+              menuItemsGroups && menuItemsGroups.length > 0 ? menuItemsGroups : undefined
+            }
+            class="max-w-80 overflow-auto bg-background"
+          /> */}
+        </div>
+        <button
+          autoFocus
+          onClick$={() => (isSidebarOpenedSig.value = false)}
+          class="absolute right-6 top-[26px]"
+        >
+          {/* <LuX class="h-8 w-8" /> */}
+        </button>
+      </Modal.Panel>
+    </Modal.Root>
+  );
+});
+
+const DarkModeToggle = component$<PropsOf<typeof Button>>(({ ...props }) => {
+  const { themeSig } = useTheme();
+  const switchLightDark = $((input: string | string[]): string | string[] | undefined => {
+    const switchWord = (word: string): string =>
+      word.includes('light')
+        ? word.replace('light', 'dark')
+        : word.replace('dark', 'light');
+    if (typeof input === 'string') {
+      return switchWord(input);
+    } else if (Array.isArray(input)) {
+      return input.map((item) => switchWord(item));
+    }
+  });
+  return (
+    <Button
+      {...props}
+      aria-label="Toggle dark mode"
+      size="icon"
+      look="ghost"
+      onClick$={async () =>
+        (themeSig.value = await switchLightDark(themeSig.value || 'light'))
+      }
+    >
+      <div class="hidden dark:block">
+        {/* <LuMoon class="h-6 w-6" /> */}
       </div>
-    </header>
+      <div class="block dark:hidden">
+        {/* <LuSun class="h-6 w-6" /> */}
+      </div>
+    </Button>
   );
 });
