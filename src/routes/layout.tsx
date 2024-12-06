250680543cbb9440cdd732b6b283261e3b758eea
James <jamesandrewpike@gmail.com>
2024-12-06 14:58:50 -0800

    commit

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -2,7 +2,7 @@ import { component$, Slot, useContextProvider } from "@builder.io/qwik";
 import { APP_STATE_CONTEXT_ID } from "~/_state/app-state-context-id";
 import { AppState } from "~/_state/app-state.type";
 
-import Footer from "~/components/widgets/Footer";
+// import Footer from "~/components/widgets/Footer";
 import Header from "~/components/widgets/Header";
 
 export default component$(() => {
@@ -22,7 +22,7 @@ export default component$(() => {
       <main>
         <Slot />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 });
