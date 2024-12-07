53ead2f54cf68edd354fd230bb715f6ce423fc23
James <jamesandrewpike@gmail.com>
2024-12-07 14:18:51 -0800

    commit

--- src/routes/(contact)/contact/index.tsx
+++ src/routes/(contact)/contact/index.tsx
@@ -1,11 +1,11 @@
 import { component$ } from "@builder.io/qwik";
+import Grid2 from "~/components/widgets/Grid2";
 
-import GridTabs from "~/components/widgets/GridTabs";
 
 export default component$(() => {
   return (
     <>
-      <GridTabs highlight="Get In Touch"
+      <Grid2 highlight="Get In Touch"
         title="Contact Us For A Consultation"
         subtitle="We're glad to learn about your business and discuss possible options."
          items={[ {
