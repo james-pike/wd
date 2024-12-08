c0cf5f935b18b9dcbdf9124138896db056704d60
James <jamesandrewpike@gmail.com>
2024-12-07 17:30:22 -0800

    commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -17,7 +17,6 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 import { SITE } from "~/config.mjs";
 import Grid2 from "~/components/widgets/Grid2";
-import Test2 from "~/components/Test2";
 
 export default component$(() => {
   return (
