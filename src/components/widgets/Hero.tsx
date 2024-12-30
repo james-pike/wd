a0f18a1766684466bfe69193a4412feed1d101d9
James <jamesandrewpike@gmail.com>
2024-12-29 19:44:31 -0500

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -9,9 +9,9 @@ export default component$(() => {
           <div class="pt-0 md:pt-[76px] pointer-events-none"></div>
           <div class="pt-8 md:py-20 lg:py-0 lg:flex lg:items-center lg:h-screen lg:gap-8">
             <div class="basis-1/2 text-center lg:text-left pb-10 md:pb-16 mx-auto">
-              <h1 class="text-5xl md:text-6xl font-bold leading-tighter tracking-tighter mb-4 font-heading dark:text-gray-200">
-                $Froppy <br class="hidden lg:block" />{" "}
-                <span class="text-sky-500">The Kaspian Frog</span>
+              <h1 class="text-5xl md:text-6xl font-bold leading-tighter tracking-tighter text-primary mb-4 font-heading dark:text-gray-200">
+                Build A Better Website <br class="hidden lg:block" />{" "}
+                {/* <span class="text-sky-500">The Kaspian Frog</span> */}
               </h1>
               <div class="max-w-3xl mx-auto lg:max-w-none">
                 <p class="text-xl mb-6 dark:text-slate-300">
