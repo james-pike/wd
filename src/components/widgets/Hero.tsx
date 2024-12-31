4c8d614f8a1e94bb40228b86f94b5b15b850318a
James <jamesandrewpike@gmail.com>
2024-12-31 14:38:27 -0500

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -3,7 +3,7 @@ import AuditPopover from "./AuditPopover";
 
 export default component$(() => {
   return (
-       <div class="relative bg-white/80 dark:bg-gray-900/80 overflow-hidden flex items-center justify-center mx-1.5 my-1 pb-1 border-radius-dot-25 rounded-lg">
+       <div class="relative bg-white/80 dark:bg-gray-900/80 overflow-hidden flex items-center justify-center mx-1.5 mt-1 mb-2 border-radius-dot-25 rounded-lg">
 
 
     <section class="relative rounded-lg  md:-mt-[76px] not-prose mx-1.5 my-1.5 bg-white/90 dark:bg-gray-900/90 border-radius-dot-25">
@@ -12,8 +12,8 @@ export default component$(() => {
         
         <div class="relative max-w-7xl mx-auto px-4 sm:px-10 ">
           <div class="pt-0 md:pt-[76px] pointer-events-none"></div>
-          <div class="pt-8 md:py-20 lg:py-0 lg:flex lg:items-center lg:h-screen lg:gap-8">
-            <div class="basis-1/2 text-center lg:text-left pb-10 md:pb-16 mx-auto">
+          <div class="pt-6 md:py-20 lg:py-0 lg:flex lg:items-center lg:h-screen lg:gap-8">
+            <div class="basis-1/2 text-center lg:text-left pb-8 md:pb-16 mx-auto">
            <AuditPopover/>
               <h1 class="text-5xl md:text-7xl font-bold leading-tighter tracking-tighter text-primary mb-4 font-heading dark:text-gray-200">
                 Build A Better Website <br class="hidden lg:block" />{" "}
