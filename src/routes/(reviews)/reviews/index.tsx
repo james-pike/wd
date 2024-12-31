1d99c3968eb55e98a434f1162d8d4cfe2f619e44
James <jamesandrewpike@gmail.com>
2024-12-31 15:46:51 -0500

    commit

--- src/routes/(reviews)/reviews/index.tsx
+++ src/routes/(reviews)/reviews/index.tsx
@@ -1,6 +1,5 @@
 import { component$ } from "@builder.io/qwik";
 import Reviews from "~/components/widgets/Reviews";
-import TestimonialCarousel from "~/components/widgets/TestimonialCarousel";
 
 
 
