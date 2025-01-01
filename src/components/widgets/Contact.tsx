ccbe15f416cd3a36e1f99a1f898fc1224162f3a1
James <jamesandrewpike@gmail.com>
2025-01-01 11:06:23 -0500

    commit

--- src/components/widgets/Contact.tsx
+++ src/components/widgets/Contact.tsx
@@ -29,10 +29,40 @@ export default component$((props: Props) => {
         <div class="relative max-w-4xl mx-auto px-1.5 py-1.5 lg:px-8">
           <div class="max-w-3xl mx-auto lg:mt-24 bg-white/95 dark:bg-slate-900/95 rounded-lg pt-6 p-3 border-radius-dot-25">
             <Headline title={title} subtitle={subtitle} highlight={highlight} classes={classes?.headline} />
-            <Separator class="my-4" />
+            <dl class="mt-10 flex flex-wrap gap-x-10 gap-y-6 text-base/7 text-gray-600">
+  <div class="flex items-start gap-x-4">
+    <dt class="flex-none">
+      <span class="sr-only">Address</span>
+      <svg class="h-7 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" />
+      </svg>
+    </dt>
+    <dd>545 Mavis Island<br/>Chicago, IL 99191</dd>
+  </div>
+  <div class="flex items-start gap-x-4">
+    <dt class="flex-none">
+      <span class="sr-only">Telephone</span>
+      <svg class="h-7 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+      </svg>
+    </dt>
+    <dd><a class="hover:text-gray-900" href="tel:+1 (555) 234-5678">+1 (555) 234-5678</a></dd>
+  </div>
+  <div class="flex items-start gap-x-4">
+    <dt class="flex-none">
+      <span class="sr-only">Email</span>
+      <svg class="h-7 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+      </svg>
+    </dt>
+    <dd><a class="hover:text-gray-900" href="mailto:hello@example.com">hello@example.com</a></dd>
+  </div>
+  
+</dl>
 
 
-            <form action="#" class="grid max-w-screen-md grid-cols-1 mx-auto gap-x-8 gap-y-6 sm:grid-cols-2">
+
+            <form action="#" class="grid max-w-screen-md pt-8 grid-cols-1 mx-auto gap-x-8 gap-y-6 sm:grid-cols-2">
               <div>
                 <label for="first-name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                   First name
