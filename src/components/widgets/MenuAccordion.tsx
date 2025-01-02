6c403e76314d0d8509872265a53448ef7ee9ae94
James <jamesandrewpike@gmail.com>
2025-01-02 00:32:40 -0500

    commit

--- src/components/widgets/MenuAccordion.tsx
+++ src/components/widgets/MenuAccordion.tsx
@@ -5,8 +5,8 @@ export default component$(() => {
   return (
     <Accordion.Root class="w-full px-5 dark:text-gray-50  ">
       {/* Services Section */}
-      <Accordion.Item>
-        <Accordion.Trigger class="text-xl flex justify-between hover:text-primary items-center py-3 border-b border-gray-300 dark:border-gray-700">
+      <Accordion.Item class="dark:border-gray-700">
+        <Accordion.Trigger class="text-xl flex trigger-text justify-between hover:text-primary items-center py-3 border-b border-gray-300 dark:border-gray-700">
           <span>About</span>
         </Accordion.Trigger>
         <Accordion.Content class=" py-2">
@@ -26,47 +26,7 @@ export default component$(() => {
         </Accordion.Content>
       </Accordion.Item>
 
-      <Accordion.Item>
-        <Accordion.Trigger class="text-xl flex justify-between hover:text-primary items-center py-3 border-b border-gray-300 dark:border-gray-700">
-          <span>About</span>
-        </Accordion.Trigger>
-        <Accordion.Content class=" py-2">
-          {/* Service Links with Highlight and Divider */}
-          <div class="hover:bg-gray-100 dark:hover:bg-primary transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/about" class="block text-lg py-3 px-2">Web Design</a>
-          </div>
-          <div class="hover:bg-gray-100 dark:hover:bg-primary transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/development" class="block text-lg py-3 px-2">Development</a>
-          </div>
-          <div class="hover:bg-gray-100 dark:hover:bg-primary transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/seo" class="block text-lg py-3 px-2">SEO Optimization</a>
-          </div>
-          <div class="hover:bg-gray-100 dark:hover:bg-primary transition">
-            <a href="/branding" class="block text-lg py-3 px-2">Brand Strategy</a>
-          </div>
-        </Accordion.Content>
-      </Accordion.Item>
-
-      <Accordion.Item>
-        <Accordion.Trigger class="text-xl flex justify-between hover:text-primary items-center py-3 border-b border-gray-300 dark:border-gray-700">
-          <span>About</span>
-        </Accordion.Trigger>
-        <Accordion.Content class=" py-2">
-          {/* Service Links with Highlight and Divider */}
-          <div class="hover:bg-gray-100 dark:hover:bg-primary transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/about" class="block text-lg py-3 px-2">Web Design</a>
-          </div>
-          <div class="hover:bg-gray-100 dark:hover:bg-primary transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/development" class="block text-lg py-3 px-2">Development</a>
-          </div>
-          <div class="hover:bg-gray-100 dark:hover:bg-primary transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/seo" class="block text-lg py-3 px-2">SEO Optimization</a>
-          </div>
-          <div class="hover:bg-gray-100 dark:hover:bg-primary transition">
-            <a href="/branding" class="block text-lg py-3 px-2">Brand Strategy</a>
-          </div>
-        </Accordion.Content>
-      </Accordion.Item>
+ 
 
     </Accordion.Root>
   );
