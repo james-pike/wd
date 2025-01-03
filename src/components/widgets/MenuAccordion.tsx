d5475a1b9ab149ff8929a3d30c4125f1a1020bd8
James <jamesandrewpike@gmail.com>
2025-01-02 22:00:39 -0500

    commit

--- src/components/widgets/MenuAccordion.tsx
+++ src/components/widgets/MenuAccordion.tsx
@@ -12,16 +12,16 @@ export default component$(() => {
         <Accordion.Content class=" py-2">
           {/* Service Links with Highlight and Divider */}
           <div class="hover:bg-primary/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/about" class="block text-lg py-3 px-2">Web Design</a>
+            <a href="/" class="block text-lg py-3 px-2">Web Design</a>
           </div>
           <div class="hover:bg-primary/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/development" class="block text-lg py-3 px-2">Development</a>
+            <a href="/" class="block text-lg py-3 px-2">Development</a>
           </div>
           <div class="hover:bg-primary/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/seo" class="block text-lg py-3 px-2">SEO Optimization</a>
+            <a href="/" class="block text-lg py-3 px-2">SEO Optimization</a>
           </div>
           <div class="hover:bg-primary/35 transition">
-            <a href="/branding" class="block text-lg py-3 px-2">Brand Strategy</a>
+            <a href="/" class="block text-lg py-3 px-2">Brand Strategy</a>
           </div>
         </Accordion.Content>
       </Accordion.Item>
@@ -33,16 +33,16 @@ export default component$(() => {
         <Accordion.Content class=" py-2">
           {/* Service Links with Highlight and Divider */}
           <div class="hover:bg-primary/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/about" class="block text-lg py-3 px-2">Web Design</a>
+            <a href="/" class="block text-lg py-3 px-2">Web Design</a>
           </div>
           <div class="hover:bg-primary/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/development" class="block text-lg py-3 px-2">Development</a>
+            <a href="/" class="block text-lg py-3 px-2">Development</a>
           </div>
           <div class="hover:bg-primary/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/seo" class="block text-lg py-3 px-2">SEO Optimization</a>
+            <a href="/" class="block text-lg py-3 px-2">SEO Optimization</a>
           </div>
           <div class="hover:bg-primary/35 transition">
-            <a href="/branding" class="block text-lg py-3 px-2">Brand Strategy</a>
+            <a href="/" class="block text-lg py-3 px-2">Brand Strategy</a>
           </div>
         </Accordion.Content>
       </Accordion.Item>
@@ -55,16 +55,16 @@ export default component$(() => {
         <Accordion.Content class=" py-2">
           {/* Service Links with Highlight and Divider */}
           <div class="hover:bg-primary/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/about" class="block text-lg py-3 px-2">Web Design</a>
+            <a href="/" class="block text-lg py-3 px-2">Web Design</a>
           </div>
           <div class="hover:bg-primary/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/development" class="block text-lg py-3 px-2">Development</a>
+            <a href="/" class="block text-lg py-3 px-2">Development</a>
           </div>
           <div class="hover:bg-primary/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/seo" class="block text-lg py-3 px-2">SEO Optimization</a>
+            <a href="/" class="block text-lg py-3 px-2">SEO Optimization</a>
           </div>
           <div class="hover:bg-primary/35 transition">
-            <a href="/branding" class="block text-lg py-3 px-2">Brand Strategy</a>
+            <a href="/" class="block text-lg py-3 px-2">Brand Strategy</a>
           </div>
         </Accordion.Content>
       </Accordion.Item>
@@ -76,16 +76,16 @@ export default component$(() => {
         <Accordion.Content class=" py-2">
           {/* Service Links with Highlight and Divider */}
           <div class="hover:bg-primary/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/about" class="block text-lg py-3 px-2">Web Design</a>
+            <a href="/" class="block text-lg py-3 px-2">Web Design</a>
           </div>
           <div class="hover:bg-primary/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/development" class="block text-lg py-3 px-2">Development</a>
+            <a href="/" class="block text-lg py-3 px-2">Development</a>
           </div>
           <div class="hover:bg-primary/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/seo" class="block text-lg py-3 px-2">SEO Optimization</a>
+            <a href="/" class="block text-lg py-3 px-2">SEO Optimization</a>
           </div>
           <div class="hover:bg-primary/35 transition">
-            <a href="/branding" class="block text-lg py-3 px-2">Brand Strategy</a>
+            <a href="/" class="block text-lg py-3 px-2">Brand Strategy</a>
           </div>
         </Accordion.Content>
       </Accordion.Item>
@@ -98,16 +98,16 @@ export default component$(() => {
         <Accordion.Content class=" py-2">
           {/* Service Links with Highlight and Divider */}
           <div class="hover:bg-primary/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/about" class="block text-lg py-3 px-2">Web Design</a>
+            <a href="/" class="block text-lg py-3 px-2">Web Design</a>
           </div>
           <div class="hover:bg-primary/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/development" class="block text-lg py-3 px-2">Development</a>
+            <a href="/" class="block text-lg py-3 px-2">Development</a>
           </div>
           <div class="hover:bg-primary/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/seo" class="block text-lg py-3 px-2">SEO Optimization</a>
+            <a href="/" class="block text-lg py-3 px-2">SEO Optimization</a>
           </div>
           <div class="hover:bg-primary/35 transition">
-            <a href="/branding" class="block text-lg py-3 px-2">Brand Strategy</a>
+            <a href="/" class="block text-lg py-3 px-2">Brand Strategy</a>
           </div>
         </Accordion.Content>
       </Accordion.Item>
