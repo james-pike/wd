63230fcd4f99fedfed97bbdde9f6f1a45d3a3777
James <jamesandrewpike@gmail.com>
2025-01-05 20:30:47 -0500

    commit

--- src/routes/(resources)/resources/index.tsx
+++ src/routes/(resources)/resources/index.tsx
@@ -6,9 +6,9 @@ import Audit from "~/components/widgets/Audit";
 export default component$(() => {
   return (
     <>
-      <Audit highlight="Reviews"
-        title="Reviews From Our Clients"
-        subtitle="Case studies and testimonials from our past work."
+      <Audit highlight="Website Audit"
+        title="Let's analyze your current site"
+        subtitle="Enter your web domain and we'll email you a performance audit."
          items={[ {
 
           },
