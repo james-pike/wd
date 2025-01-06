8fe56e897c8a9e2a9a91d263ea934e7b047be97f
James <jamesandrewpike@gmail.com>
2025-01-05 22:53:19 -0500

    commit

--- src/components/widgets/MenuAccordion.tsx
+++ src/components/widgets/MenuAccordion.tsx
@@ -59,8 +59,8 @@ export default component$(() => {
 
       <Accordion.Item class="dark:border-gray-700">
         <Accordion.Trigger class="text-xl flex trigger-text justify-between hover:text-primary items-center py-3 border-b border-gray-300 dark:border-gray-700">
-        <a href="/contact" class=" hover:underline">
-  Contact Us
+        <a href="/pricing" class=" hover:underline">
+  Pricing
 </a>  
         </Accordion.Trigger>
        
